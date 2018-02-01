9270dea6976a80afee4f6a970fd411cd8539e67a
health4geeks85 <health4geeks85@gmail.com>
2018-02-01 09:37:22 +0700

    ADD: atexit use

--- gracefullKiller.py
+++ gracefullKiller.py
@@ -1,6 +1,7 @@
 import signal
 import time
 import requests
+import atexit
 
 
 class GracefulKiller:
@@ -9,6 +10,7 @@ class GracefulKiller:
     def __init__(self):
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
+        signal.signal(signal.SIGHUP, self.exit_gracefully)
 
     def exit_gracefully(self, signum, frame):
         self.killNow = True
@@ -16,12 +18,18 @@ class GracefulKiller:
 
 if __name__ == "__main__":
     killer = GracefulKiller()
+    def setKiller():
+        killer.killNow = True
+
+    atexit.register(setKiller)
+
     while True:
         time.sleep(1)
         print("do something in a loop")
         if killer.killNow:
             requests.get("http://127.0.0.1:8000/kill")
             print("I was killed")
+            time.sleep(1)
             break
 
     print("End of the program")
