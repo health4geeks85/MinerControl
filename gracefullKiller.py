25a633ccf46cf26eb9929fbffd88c2f374dedf0d
health4geeks85 <health4geeks85@gmail.com>
2018-02-01 10:12:00 +0700

    ADD: additional windows signal capture

--- gracefullKiller.py
+++ gracefullKiller.py
@@ -1,8 +1,7 @@
 import signal
 import time
 import requests
-import atexit
-
+import win32api
 
 class GracefulKiller:
     killNow = False
@@ -10,21 +9,34 @@ class GracefulKiller:
     def __init__(self):
         signal.signal(signal.SIGINT, self.exit_gracefully)
         signal.signal(signal.SIGTERM, self.exit_gracefully)
-        signal.signal(signal.SIGHUP, self.exit_gracefully)
+        # windows
+        signal.signal(signal.SIGABRT, self.exit_gracefully)
+        signal.signal(signal.SIGSEGV, self.exit_gracefully)
+        signal.signal(signal.SIGILL, self.exit_gracefully)
+        signal.signal(signal.SIGFPE, self.exit_gracefully)
+
+        win32api.SetConsoleCtrlHandler(self.win_exit, True)
+        
+       # signal.signal(signal.SIGHUP, self.exit_gracefully)
 
     def exit_gracefully(self, signum, frame):
+        self.killNow = True
+
+    def win_exit(self, func=None):
         self.killNow = True
 
 
 if __name__ == "__main__":
     killer = GracefulKiller()
-    def setKiller():
-        killer.killNow = True
-
-    atexit.register(setKiller)
+    requests.get("http://127.0.0.1:8000/start")
+    
 
     while True:
-        time.sleep(1)
+        try:
+            time.sleep(1)
+        except InterruptedError:
+            killer.killNow = True
+            
         print("do something in a loop")
         if killer.killNow:
             requests.get("http://127.0.0.1:8000/kill")
